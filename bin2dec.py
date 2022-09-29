273eaa80e3caf8337898024c2d34ea770e2bbc8b
Josepct2001 <al427465@uji.es>
2022-09-29 19:07:40 +0200

    Josep

---
+++ bin2dec.py
@@ -0,0 +1,13 @@
+#cambio maria
+def binario_a_decimal(binario):
+    posicion = 0
+    decimal = 0
+    binario = binario[::-1]
+    for digito in binario:
+
+        multiplicador = 2**posicion
+        decimal += int(digito) * multiplicador
+        posicion += 1
+    return decimal
+    #cambiodeprueba
+    
